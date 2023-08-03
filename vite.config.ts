9ffa8f29ad57e1e6ef11906dd3dc141795560490
Sandesh Hyoju <sundesz@gmail.com>
2023-08-03 20:04:59 +0300

    Change vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig, loadEnv } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+// https://stackoverflow.com/questions/66389043/how-can-i-use-vite-env-variables-in-vite-config-js
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
+
+  return defineConfig({
+    define: {
+      'process.env': process.env,
+    },
+    server: {
+      port: 3000,
+      proxy: {
+        '/api/v1': {
+          target: `${process.env.VITE_BACKEND_URL}`,
+          changeOrigin: true,
+        },
+      },
+    },
+    build: {
+      outDir: '../dist',
+    },
+
+    plugins: [react()],
+  })
+}
