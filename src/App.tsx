347e1f4922033ac86fa68cdc425497de985663a4
Sandesh Hyoju <sundesz@gmail.com>
2023-08-03 20:02:54 +0300

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
+import Home from './components/Home'
+import Layout from './components/Layout'
+import Page404 from './components/Page404'
+import RecipeForm from './components/RecipeForm'
+import RecipeItem from './components/RecipeItem'
+import RecipeList from './components/RecipeList'
+import 'react-toastify/dist/ReactToastify.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App: React.FC = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Router>
+      <ToastContainer
+        position='top-center'
+        autoClose={1000}
+        style={{ width: '600px' }}
+      />
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route index element={<Home />} />
+
+          <Route path='recipes'>
+            <Route index element={<RecipeList />} />
+            <Route path=':id' element={<RecipeItem />} />
+            <Route path='new' element={<RecipeForm />} />
+          </Route>
+
+          <Route path='*' element={<Page404 />} />
+        </Route>
+      </Routes>
+    </Router>
   )
 }
 
