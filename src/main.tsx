a7798dd6e5d452acaeb080f281b04378dd135081
Sandesh Hyoju <sundesz@gmail.com>
2023-08-03 20:06:52 +0300

    Add context

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import { RecipeProvider } from './RecipeContext'
+import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <RecipeProvider>
+      <App />
+    </RecipeProvider>
+  </React.StrictMode>
 )
